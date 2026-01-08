6c2caeb147e05e2f5961af0d513ac50b44f83a77
yajatkaul <mc.crafter1973@gmail.com>
2026-01-08 12:32:26 +0530

    Shifting from cobblemon to msd namespace

--- common/src/main/java/com/github/yajatkaul/mega_showdown/mixin/client/LivingEntityRendererMixin.java
+++ common/src/main/java/com/github/yajatkaul/mega_showdown/mixin/client/LivingEntityRendererMixin.java
@@ -17,6 +17,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 
 import java.util.Optional;
 
